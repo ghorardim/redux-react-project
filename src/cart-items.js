4ca00c71ed0227e063fffcec1c47631631c5306a
abdullah.mahmud <abdullah.mahmud@bjitgroup.com>
2022-11-11 17:48:07 +0600

    project setup, display image have a issue

---
+++ src/cart-items.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img:
+      "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    amount: 1
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img:
+      "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    amount: 1
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img:
+      "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    amount: 1
+  }
+];
