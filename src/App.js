afb147a312c26b6fd68dbdedc3cde34cd24d0d49
abdullah.mahmud <abdullah.mahmud@bjitgroup.com>
2022-11-14 11:23:22 +0600

    done 30.00 min

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import { createStore } from "redux";
 
+const initialStore = {
+  count: 0,
+}
+// reducer
+function reducer(state, action) {
+  console.log({state, action});
+  return state;
+}
+// store
+const store = createStore(reducer, initialStore);
+store.dispatch({type: 'DECREASE'});
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()}/>
       <CartContainer cart={cartItems} />
     </main>
   );
